8ac937652063db5628428d46c9c1a73456d9c54e
Anjali P <anjalip1523@gmail.com>
2026-07-16 23:03:24 +0530

    Add project main menu

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print("Welcome to AI-Powered Student Placement Tracker")
+from modules.menu import show_menu
+
+while True:
+    show_menu()
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Add Student feature coming soon...")
+
+    elif choice == "2":
+        print("View Students feature coming soon...")
+
+    elif choice == "3":
+        print("Search Student feature coming soon...")
+
+    elif choice == "4":
+        print("Update Student feature coming soon...")
+
+    elif choice == "5":
+        print("Delete Student feature coming soon...")
+
+    elif choice == "6":
+        print("Thank you for using AI-Powered Student Placement Tracker.")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
---
+++ modules/menu.py
@@ -0,0 +1,13 @@
+def show_menu():
+
+    print("\n====================================================")
+    print("AI-Powered Student Placement Tracker")
+    print("\n====================================================")
+    
+    print("1.add student")
+    print("2.view student")
+    print("3.search student")
+    print("4.update student")
+    print("5.delete student")
+    print("6.exit")
+
